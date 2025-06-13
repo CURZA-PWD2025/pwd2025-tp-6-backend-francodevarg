0115e0d456e77c94e3d7b377c4b3394178566fb1
Franco Narvaez <juanfranconarvaez@gmail.com>
2025-06-13 16:09:30 -0300

    Backend: Habilito CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from .modules.articulo.articulo_routes import articulo_bp
 from .modules.categoria.categoria_routes import categoria_bp
 from .modules.proveedor.proveedor_routes import proveedor_bp
@@ -6,6 +7,7 @@ from .modules.marca.marca_routes import marca_bp
 
 def create_app():
     app = Flask(__name__)
+    CORS(app) 
     app.register_blueprint(articulo_bp)
     app.register_blueprint(categoria_bp)
     app.register_blueprint(marca_bp)
