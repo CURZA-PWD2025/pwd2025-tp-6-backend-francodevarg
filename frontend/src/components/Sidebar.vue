9f529eee61cafd076f3a494f79ad8c3ff87793df
Franco Narvaez <juanfranconarvaez@gmail.com>
2025-06-24 16:22:15 -0300

    Mejoro el sidebar con router link

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,27 +1,24 @@
 <template>
-    <v-navigation-drawer app permanent>
-      <v-list nav dense>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          :to="item.to"
-          router
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </template>
-  
-  <script setup lang="ts">
-  const items = [
-    { title: 'Home', icon: 'mdi-home', to: '/' },
-    { title: 'Profile', icon: 'mdi-account', to: '/profile' },
-    { title: 'Settings', icon: 'mdi-cog', to: '/settings' },
-  ]
-  </script>
-  
+  <v-navigation-drawer app permanent>
+    <v-list nav dense>
+      <v-list-item 
+        v-for="item in items" 
+        :key="item.title" 
+        :to="item.to" 
+        router link 
+        :prepend-icon="item.icon">
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup lang="ts">
+const items = [
+  { title: 'Marcas', icon: 'mdi-tag-multiple', to: '/marcas' },
+  { title: 'Categorías', icon: 'mdi-shape', to: '/categorias' },
+  { title: 'Proveedores', icon: 'mdi-truck-delivery', to: '/proveedores' },
+  { title: 'Artículos', icon: 'mdi-cube-outline', to: '/articulos' },
+]
+</script>
